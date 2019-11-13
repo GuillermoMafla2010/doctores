90191e63900ac0b8a01c27f716340fe4615adb95
GuillermoMafla <cjativa12@hotmail.com>
2019-11-12 22:17:15 -0500

    pacientes por id

--- BackEnd/rest_api/pacientesController.js
+++ BackEnd/rest_api/pacientesController.js
@@ -3,6 +3,8 @@ const server=require('../server')
 const models=require('../models/index')
 const cors=require('cors')
 
+
+//Metodo que retorna a todos los pacientes
 server.get("/pacientes",(req,res)=>{
     
     models.Pacientes.findAll().then(pacientes=>{
@@ -11,5 +13,13 @@ server.get("/pacientes",(req,res)=>{
         })
     })
 
+})
+
+
+//Metodo que retorna un paciente segun su numero id
+
+server.get("/pacientes/:id",(req,res)=>{
+    let id=req.params.id
+    models.Pacientes.findAll({/*include:[{all:true,nested:true}] ,*/where:{id:id}}).then(paciente=>{res.json({paciente})})
 })
 
